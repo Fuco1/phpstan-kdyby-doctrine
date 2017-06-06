2ceca1b14392942e494dce91626e9464c4d4c410
Matus Goljer <matus.goljer@gmail.com>
2017-06-06 14:39:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Kdyby/Doctrine extensions for PHPStan
+
+* [PHPStan](https://github.com/phpstan/phpstan)
+* [Kdyby/Doctrine](https://github.com/Kdyby/Doctrine)
+
+This extension provides following features:
+
+* Provides correct return type for `Kdyby\Doctrine\EntityManager::find`, `getReference`,  `getPartialReference` and `getRepository` when `Foo::class` entity class name is provided as the first argument
+
+## Usage
+
+To use this extension, require it in [Composer](https://getcomposer.org/):
+
+```
+composer require --dev phpstan/phpstan-kdyby-doctrine
+```
+
+And include extension.neon in your project's PHPStan config:
+
+```
+includes:
+    - vendor/phpstan/phpstan-doctrine-kdyby/extension.neon
+```
---
+++ src/Type/Kdyby/Doctrine/EntityManagerDynamicReturnTypeExtension.php
@@ -0,0 +1,67 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Type\Kdyby\Doctrine;
+
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeCombinator;
+
+class EntityManagerDynamicReturnTypeExtension implements \PHPStan\Type\DynamicMethodReturnTypeExtension
+{
+
+    public static function getClass(): string
+    {
+        return \Kdyby\Doctrine\EntityManager::class;
+    }
+
+    public function isMethodSupported(MethodReflection $methodReflection): bool
+    {
+        return in_array($methodReflection->getName(), [
+            'find',
+            'getReference',
+            'getPartialReference',
+            'getRepository',
+        ], true);
+    }
+
+    public function getTypeFromMethodCall(
+        MethodReflection $methodReflection,
+        MethodCall $methodCall,
+        Scope $scope
+    ): Type
+    {
+        if (count($methodCall->args) === 0) {
+            return $methodReflection->getReturnType();
+        }
+        $arg = $methodCall->args[0]->value;
+        if (!($arg instanceof \PhpParser\Node\Expr\ClassConstFetch)) {
+            return $methodReflection->getReturnType();
+        }
+
+        $class = $arg->class;
+        if (!($class instanceof \PhpParser\Node\Name)) {
+            return $methodReflection->getReturnType();
+        }
+
+        $class = (string) $class;
+
+        if ($class === 'static') {
+            return $methodReflection->getReturnType();
+        }
+
+        if ($class === 'self') {
+            $class = $scope->getClassReflection()->getName();
+        }
+
+        $type = new ObjectType($class);
+        if ($methodReflection->getName() === 'find') {
+            $type = TypeCombinator::addNull($type);
+        }
+
+        return $type;
+    }
+
+}
